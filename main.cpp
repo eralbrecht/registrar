0b37877a883e7456757368ce3607da60a3cb811f
Jordan Farmer <farmer@chapman.edu>
2020-04-22 21:37:05 +0000

    Commiting before merge.

--- main.cpp
+++ main.cpp
@@ -1,28 +1,44 @@
 #include <iostream>
+#include <fstream>
 #include "Queue.h"
 #include "Student.h"
 #include "Window.h"
+
+
 using namespace std;
+
 int main(int argc, char** argv)
 {
 
-	string analysisString;
+	string simulationFile;
 	int totalStudentCount = 0;
 	//Student newStudent; i think we make instances of this later and dont need it up here
 	//loads in file
-	while (analysisString.length() == 0)
+
+	//This whole chunk is to make sure the user provided a file, and checks if the file exists
+	if (argc > 1)
 	{
-		if (argc > 1)
+		string inputFile = argv[1];
+		cout << "File name is: " << inputFile << endl;
+		simulationFile = inputFile;
+
+		ifstream inputFile;
+		inputFile.open(simulationFile);
+
+		if(!inputFile)
 		{
-			string inputFile = argv[1];
-			cout << "file name is: " << inputFile << endl;
-			analysisString = inputFile;
-		}
-		else
-		{
-			cout << "you must give a file name as your command line argument when calling this function, try again" << endl;
+			cout << "This file does not exist in the directory. Pleas try again." << endl;
+			exit(1);
 		}
 	}
+	else
+	{
+		cout << "You must give a file name as your command line argument when running this program, try again." << endl;
+		exit(1);
+	}
+
+	//If we reach this point, the file exists and the simulation can be started
+
 	int lineCount = 0;
 	string currString;
 	//we will iterate through the file, in waves of information, to create windows, and students
